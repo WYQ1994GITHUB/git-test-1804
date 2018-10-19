321328f89997a3071ae28242ed36c125c7072128
WYQ1994GITHUB <44285081+WYQ1994GITHUB@users.noreply.github.com>
2018-10-19 17:31:36 +0800

    Update TestController.java

--- src/main/java/com/baizhi/entity/TestController.java
+++ src/main/java/com/baizhi/entity/TestController.java
@@ -7,5 +7,6 @@ public class TestController {
     public static void main(String[] args) {
         System.out.println("哈哈哈");
         System.out.println("唧唧复唧唧");
+        System.out.println("呵呵哒");
     }
 }
